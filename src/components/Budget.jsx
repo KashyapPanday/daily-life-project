15b49fe39e66e64ab3a60c592ca0fed704bf54fc
Kashyap <kashintouch@gmail.com>
2022-08-28 00:26:23 -0400

    finished rendering workout and budget arrays
    Signed-off-by: Kashyap <kashintouch@gmail.com>

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-//const budgetCategories = ["Gas","Primerica","Shopping","Restaurant","Lunch","Fun","Parents","Student Loans","Bike Fund",];
+const budgetCategories = ["Gas","Primerica","Shopping","Restaurant","Lunch","Fun","Parents","Student Loans","Bike Fund",];
 
 
 export const Budget = () => {
     return (
         <div>
+            <div>
             Budget Section
+            </div>
+        <table>
+            <tr>{budgetCategories.map(function (value, index) {return <th key={index}>{value}</th>})}</tr>
+        </table>
         </div>
     )
 }
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
+const workoutCategories = ['DATE','EXERCISE','TIME','COMMENTS']
+const prRecord = ['Squat', 'Bench', 'Deadlift']
 
 
 export const Workout = () => {
     return (
         <div>
-            Workout Section
+        <div>Workout Log</div>
+        <table>
+            <tr>
+            {prRecord.map((value,index) => <th key={index}>{value}</th> )}
+            </tr>
+        </table>
+        <table>
+            <tr>
+                {workoutCategories.map(function (value, index){ return <th key={index}>{value}</th>})}
+            </tr>
+        </table>
         </div>
     )
 }
